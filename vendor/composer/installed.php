93fe2f456b73c091b5c4f155edaf602710252ca4
oladitisodiq <oladitisodiq@email.com>
2025-02-16 23:19:28 +0100

    composer fixed

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -5,7 +5,7 @@
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
-        'reference' => '4218976b3a6269faec74f9cb4ef898652226fe57',
+        'reference' => '2812b32806e97d741a84e07f7b4ce14193e8ec38',
         'name' => 'laravel/laravel',
         'dev' => true,
     ),
@@ -451,7 +451,7 @@
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
-            'reference' => '4218976b3a6269faec74f9cb4ef898652226fe57',
+            'reference' => '2812b32806e97d741a84e07f7b4ce14193e8ec38',
             'dev_requirement' => false,
         ),
         'laravel/pint' => array(
@@ -895,8 +895,8 @@
         'psr/log-implementation' => array(
             'dev_requirement' => false,
             'provided' => array(
-                0 => '1.0|2.0|3.0',
-                1 => '1.0.0 || 2.0.0 || 3.0.0',
+                0 => '1.0.0 || 2.0.0 || 3.0.0',
+                1 => '1.0|2.0|3.0',
             ),
         ),
         'psr/simple-cache' => array(
